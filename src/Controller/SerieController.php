d19d76fcb8a2ba12c5902017345c11fe8a00f92b
Jérôme DERYCKE <jeromederycke@gmail.com>
2023-04-05 14:19:54 +0200

    Ajout de la bdd

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
+use App\Form\SerieType;
+use App\Repository\SerieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,30 +16,87 @@ class SerieController extends AbstractController
     /**
      * @Route("/series", name="serie_list")
      */
-    public function list(): Response
+    public function list(SerieRepository $serieRepository): Response
     {
+        $serie=$serieRepository->findBestSeries();
 
-        //todo: aller chercher les series en bdd
-
-        return $this->render('serie/list.html.twig');
+        return $this->render('serie/list.html.twig',[
+            "series" => $serie
+        ]);
     }
 
     /**
      * @Route("/series/details/{id}", name="serie_details")
      */
-    public function details(int $id): Response
+    public function details(int $id, SerieRepository $serieRepository): Response
     {
+        $serie = $serieRepository->find($id);
 
-        //todo: aller chercher la serie en bdd
-
-        return $this->render('serie/details.html.twig');
+        return $this->render('serie/details.html.twig', [
+            "serie" =>$serie
+        ]);
     }
 
     /**
      * @Route("/series/create", name="serie_create")
      */
-    public function create(): Response
+    public function create(
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response
     {
+        $serie = new Serie();
+        $serie->setDateCreated(new \DateTime());
+
+        $serieForm = $this->createForm(SerieType::class, $serie);
+
+        $serieForm->handleRequest($request);
+
+        if ($serieForm->isSubmitted() && $serieForm->isValid()){
+            $entityManager->persist($serie);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Serie added! Good job.');
+            return $this->redirectToRoute('serie_details', ['id'=>$serie->getId()]);
+        }
+
+        return $this->render('serie/create.html.twig',[
+            'serieForm' => $serieForm->createView()
+        ]);
+    }
+
+    /**
+     * @Route ("/series/demo", name="serie_em-demo")
+     */
+    public function demo(EntityManagerInterface $entityManager): Response
+    {
+        //Crée une instance de mon entité
+        $serie=new Serie();
+
+        //Hydrate toutes les propriétés
+        $serie->setName('pif');
+        $serie->setBackdrop('dafsd');
+        $serie->setPoster('dafsdf');
+        $serie->setDateCreated(new \DateTime());
+        $serie->setFirstAirDate(new \DateTime("-1 year"));
+        $serie->setLastAirDate(new \DateTime("-6 month"));
+        $serie->setGenres('Drama');
+        $serie->setOverview('bla bl bla');
+        $serie->setPopularity(123.45);
+        $serie->setVote(8.2);
+        $serie->setStatus('Canceled');
+        $serie->setTmdbId(342568);
+
+        dump($serie);
+
+        $entityManager->persist($serie);
+        $entityManager->flush();
+
+        dump($serie);
+
+        $entityManager->remove($serie);
+        $entityManager->flush();
+
         return $this->render('serie/create.html.twig');
     }
 }
